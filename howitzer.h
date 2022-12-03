9b056738b8947526d538d009279f9124b77a7f37
david <davraya4@gmail.com>
2022-12-03 14:12:42 -0700

    Implemented fire, and made projectile a memeber variable in Howitzer

--- howitzer.h
+++ howitzer.h
@@ -3,8 +3,8 @@
 #include "physics.h"
 #include "projectile.h"
 #include "position.h"
+#include "uiDraw.h"
 
- 
 class Howitzer
 {
 
@@ -13,14 +13,26 @@ private:
    Physics::Angle a;
    Position position = Position(10000,10000);
    double age;
+   ogstream gout;
 
 public:
+   Howitzer() {
+      double howitzerX = position.getMetersX();
+      double howitzerY = position.getMetersY();
+      projectile.initPosition(howitzerX, howitzerX, 0);
+   }
    void moveBarrel();
    void fire();
+   void updateProjectilePosition();
    Physics::Angle* getAngle() { return &a; };
    Position getPosition() const;
    bool canFire() const;
    void setAngle();
+   Position getProjectilePosition();
+   vector<Position>::reverse_iterator getReverseIteratorBegin();
+   vector<Position>::reverse_iterator getReverseIteratorEnd();
+   void displayProjectileStatus();
+   double getProjectileAirTime();
 
 };
 
